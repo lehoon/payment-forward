31fead520cc285c28828ecdf58f6b84d9a12b107
lehoon <lehoons@gmail.com>
2023-02-25 00:20:25 +0800

    http client 发起请求需要处理服务器ip地址为0.0.0.0情况

--- payment-forward/NotifyHttpClient.cpp
+++ payment-forward/NotifyHttpClient.cpp
@@ -1,8 +1,14 @@
-#include "NotifyHttpClient.h"
+﻿#include "NotifyHttpClient.h"
 #include "httplib.h"
 
 void CNotifyHttpClient::SendShutdownRequest(std::string host, int port, std::string key) {
-	httplib::Client client(host, port);
+	std::string remoteHost = host;
+
+	if (host.compare("0.0.0.0") == 0) {
+		remoteHost = "localhost";
+	}
+
+	httplib::Client client(remoteHost, port);
 	client.set_connection_timeout(5);
 	client.set_read_timeout(5);
 	client.set_write_timeout(5);
